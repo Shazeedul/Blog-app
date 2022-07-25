0746f4ced7686ff920dcf2e06c5d8810025a2778
Shazeedul <syedshazeedul@gmail.com>
2022-07-25 23:03:58 +0600

    25-07-22

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -22,7 +22,7 @@ class CategoryController extends Controller
     
     public function store(Request $request)
     {
-        // dd($request->all());
+        dd($request->all());
         $this->validate($request, [
             'name' => 'required',
         ]);
